73c48f22e86fc2dd388f74b1b6b596b337381f00
royeradames <royeraadames@gmail.com>
2021-04-08 02:03:41 -0400

    📦compile project content

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import data from "./data";
 
 function App() {
   return (
---
+++ src/data.tsx
@@ -0,0 +1,37 @@
+// icons
+import supervisorIcon from "./images/icon-supervisor.svg";
+import teamBuilderIcon from "./images/icon-team-builder.svg";
+import karmaIcon from "./images/icon-karma.svg";
+import calculatorIcon from "./images/icon-calculator.svg";
+
+// data
+const data = {
+  title: "Reliable, efficient delivery",
+  tagLine: "Powered by Technology",
+  leadingMessage:
+    "Our Artificial Intelligence powered tools use millions of project data points to ensure that your project is successful",
+  supervisor: {
+    title: "Supervisor",
+    message: "Monitors activity to identify project roadblocks",
+    icon: supervisorIcon,
+  },
+  teamBuilder: {
+    title: "Team Builder",
+    message:
+      "Scans our talent network to create the optimal team for your project",
+    icon: teamBuilderIcon,
+  },
+  Karma: {
+    title: "Karma",
+    message: "Regularly evaluates our talent to ensure quality",
+    icon: karmaIcon,
+  },
+  Calculator: {
+    title: "Calculator",
+    message:
+      "Uses data from past projects to provide better delivery estimates",
+    icon: calculatorIcon,
+  },
+};
+
+export default data;
