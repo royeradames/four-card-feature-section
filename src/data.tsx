9a6b30501c5922663dafee198528f48771161e2d
royeradames <royeraadames@gmail.com>
2021-04-08 02:10:09 -0400

    👌make cards into a list of arrays

--- src/data.tsx
+++ src/data.tsx
@@ -10,28 +10,31 @@ const data = {
   tagLine: "Powered by Technology",
   leadingMessage:
     "Our Artificial Intelligence powered tools use millions of project data points to ensure that your project is successful",
-  supervisor: {
-    title: "Supervisor",
-    message: "Monitors activity to identify project roadblocks",
-    icon: supervisorIcon,
-  },
-  teamBuilder: {
-    title: "Team Builder",
-    message:
-      "Scans our talent network to create the optimal team for your project",
-    icon: teamBuilderIcon,
-  },
-  Karma: {
-    title: "Karma",
-    message: "Regularly evaluates our talent to ensure quality",
-    icon: karmaIcon,
-  },
-  Calculator: {
-    title: "Calculator",
-    message:
-      "Uses data from past projects to provide better delivery estimates",
-    icon: calculatorIcon,
-  },
+
+  cards: [
+    {
+      title: "Supervisor",
+      message: "Monitors activity to identify project roadblocks",
+      icon: supervisorIcon,
+    },
+    {
+      title: "Team Builder",
+      message:
+        "Scans our talent network to create the optimal team for your project",
+      icon: teamBuilderIcon,
+    },
+    {
+      title: "Karma",
+      message: "Regularly evaluates our talent to ensure quality",
+      icon: karmaIcon,
+    },
+    {
+      title: "Calculator",
+      message:
+        "Uses data from past projects to provide better delivery estimates",
+      icon: calculatorIcon,
+    },
+  ],
 };
 
 export default data;
